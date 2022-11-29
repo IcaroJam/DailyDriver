ee442be04c32d569f24f2a42d732b6d10df3216d
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2022-11-29 19:27:23 +0100

    Task Area setup B).

--- config_test.js
+++ config_test.js
@@ -6,18 +6,26 @@ if (initConf == undefined) {
 }
 localStorage.setItem("default_conf_test.json", JSON.stringify(initConf));
 
-initConf = defaults; // Overwrite local storage (should be removed once the defaults are done).
-loadSideBar(initConf["startTime"], initConf["endTime"]);
 
-function loadSideBar(sTime, eTime) {
-	// Handle bad inputs:
-	if (sTime === eTime | sTime < 0 || eTime < 0 || sTime > 23 || eTime > 23) {
-		sTime = 9;
-		eTime = 18;
+
+initConf = defaults; // Overwrite local storage (should be removed once the defaults are done).
+
+handleErrors();
+
+loadContent(initConf["startTime"], initConf["endTime"]);
+
+function handleErrors() {
+	if (initConf.startTime === initConf.endTime | initConf.startTime < 0 || initConf.endTime < 0 || initConf.startTime > 23 || initConf.endTime > 23) {
+		initConf.startTime = 9;
+		initConf.endTime = 18;
 		console.log("Bad start-end times, falling back to defaults.")
 	}
+}
 
+function loadContent(sTime, eTime) {
 	const bar = document.getElementById("sideBar");
+	const taskBar = document.getElementById("taskBar");
+	const variables = getComputedStyle(document.querySelector(":root"));
 	const timeSpan = sTime > eTime ? (24 + eTime - sTime) : eTime - sTime;
 	bar.style.height = 160 * timeSpan + 10 + "px";
 
@@ -28,6 +36,11 @@ function loadSideBar(sTime, eTime) {
 		const cellText = document.createTextNode(sTime);
 		newCell.appendChild(cellText);
 		bar.appendChild(newCell);
+
+		const newTaskBack = document.createElement("div");
+		newTaskBack.className = "taskElement";
+		newTaskBack.style.backgroundColor = sTime % 2 ? variables.getPropertyValue("--subdivColour2") : variables.getPropertyValue("--subdivColour3");
+		taskBar.appendChild(newTaskBack);
 
 		sTime++;
 		if (sTime === 24)
