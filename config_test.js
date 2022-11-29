1120bfb8b3038c5339a1b66048478f47a8f15f25
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2022-11-29 17:06:45 +0100

    SideBar json setup :D.

--- config_test.js
+++ config_test.js
@@ -1,9 +1,36 @@
-import defaults from "./default_conf_test.json" assert {type: json};
+import defaults from './default_conf_test.json' assert {type: 'json'};
 /* Get the initial config file from the local cache: */
-let	initConf = localStorage.getItem("default_conf_test.json");
-if (!initConf) {
+let	initConf = JSON.parse(localStorage.getItem("default_conf_test.json"));
 	initConf = defaults;
-	localStorage.setItem("default_conf_test.json", JSON.stringify(initConf));
+if (initConf == undefined) {
+	initConf = defaults;
 }
+localStorage.setItem("default_conf_test.json", JSON.stringify(initConf));
 
+loadSideBar(initConf["startTime"], initConf["endTime"]);
 
+function loadSideBar(sTime, eTime) {
+	// Handle bad inputs:
+	if (sTime === eTime | sTime < 0 || eTime < 0 || sTime > 23 || eTime > 23) {
+		sTime = 9;
+		eTime = 18;
+		console.log("Bad start-end times, falling back to defaults.")
+	}
+
+	const bar = document.getElementById("sideBar");
+	const timeSpan = sTime > eTime ? (24 + eTime - sTime) : eTime - sTime;
+	bar.style.height = 160 * timeSpan + 10 + "px";
+
+	// This falls one short of the endTime:
+	while (sTime !== eTime) {
+		const newCell = document.createElement("div");
+		newCell.className = "sideElement";
+		const cellText = document.createTextNode(sTime);
+		newCell.appendChild(cellText);
+		bar.appendChild(newCell);
+
+		sTime++;
+		if (sTime === 24)
+			sTime = 0;
+	}
+}
