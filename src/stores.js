160806e68336dc1f7d211cecdca53bc8a44ea933
senari <onextthenitromancer@gmail.com>
2023-10-23 13:45:32 +0200

    Comment for future reference.

--- src/stores.js
+++ src/stores.js
@@ -5,5 +5,6 @@ import { defaults as configFile } from "./defaultConf";
 export const defaults = writable(configFile);
 
 export const tasksList = writable(
+	// Retrieve the tasks from local storage ONLY if we are on clientside (svelte will throw an error if serverside since localstorage isn't defined there)
 	browser && JSON.parse(localStorage.getItem("dailydriver_tasks")) || []
 );
