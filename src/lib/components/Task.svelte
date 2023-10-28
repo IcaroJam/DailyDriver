0637bfb50b47666282a853219df3830912777a13
senari <onextthenitromancer@gmail.com>
2023-10-28 11:51:57 +0200

    Checkbox-like complete task icons.

--- src/lib/components/Task.svelte
+++ src/lib/components/Task.svelte
@@ -47,7 +47,11 @@
 			in:slide={{axis: "x", duration: 100}}
 			out:slide={{axis: "x", duration: 400}}
 			width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path d="M8 12.3333L10.4615 15L16 9M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+				{#if props.completed}
+					<path d="M8 12.3333L10.4615 15L16 9M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+				{:else}
+					<path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+				{/if}
 			</svg>
 		{/if}
 		<span>{props.description}</span>
