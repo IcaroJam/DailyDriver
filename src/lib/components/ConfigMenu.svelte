34380de2247630f953e05c061ad69a44f0f46de1
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-11-21 13:56:24 +0100

    Config form fixes.

--- src/lib/components/ConfigMenu.svelte
+++ src/lib/components/ConfigMenu.svelte
@@ -63,10 +63,10 @@
 
 
 
-<div class="blurred-window" on:submit|preventDefault={saveSettings}>
-	<form class="config-window">
+<div class="blurred-window">
+	<form class="config-window" on:submit|preventDefault={saveSettings}>
 		<div class="config-horizontal">
-			<button id="close-config-btn" title="Close settings" on:click|preventDefault={() => {configShow = false;}}>
+			<button type="button" id="close-config-btn" title="Close settings" on:click|preventDefault={() => {configShow = false;}}>
 				<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<g id="Edit / Add_Plus_Circle">
 					<path id="Vector" d="M8 12H12M12 12H16M12 12V16M12 12V8M12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
